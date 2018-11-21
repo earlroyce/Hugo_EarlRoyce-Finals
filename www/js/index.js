19a9b2f5a8d0b8690a646a6f90541bce0186262f
earlroyce <44243688+earlroyce@users.noreply.github.com>
2018-11-21 14:07:55 -0600

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -2,20 +2,25 @@
 // Modified to initialize my buttons and functions
  
 var app = {
+    // Application Constructor
     initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+        this.bindEvents();
     },
-
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-		document.getElementById("battButton").addEventListener("click", battStat);
-		document.getElementById("locationButton").addEventListener("click", geoLocation);
-		document.getElementById("vibrateButton").addEventListener("click", vibratePhone);
-		document.getElementById("cameraButton").addEventListener("click", takePhoto);
-		
-
+        app.receivedEvent('deviceready');
     },
-
+    // Update DOM on a Received Event
     receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
@@ -26,7 +31,21 @@ var app = {
 
         console.log('Received Event: ' + id);
     }
-		  
 };
 
-app.initialize();
+// Add to index.js or the first page that loads with your app.
+// For Intel XDK and please add this to your app.js.
+
+document.addEventListener('deviceready', function () {
+  // Enable to debug issues.
+  // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
+  
+  var notificationOpenedCallback = function(jsonData) {
+    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+  };
+
+  window.plugins.OneSignal
+    .startInit("b026acb3-9788-477f-91c6-b1a23ff9e805")
+    .handleNotificationOpened(notificationOpenedCallback)
+    .endInit();
+}, false);
